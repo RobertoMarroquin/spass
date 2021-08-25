58de952389e4e870c29e47b95106bf13e71d10f5
Roberto Marroquin <robma96@gmail.com>
2021-08-25 17:06:57 -0600

    cors sobre installed apps

--- visorspass/visorspass/settings.py
+++ visorspass/visorspass/settings.py
@@ -27,6 +27,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+#CORS Django Variables
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
+#CORS_ALLOWED_ORIGINS = [
+#    '*',
+#] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
+#CORS_ALLOWED_ORIGIN_REGEXES = [
+#    '*',
+#]
 
 # Application definition
 
@@ -138,12 +147,3 @@ GRAPH_MODELS = {
   'app_labels': ["visor",],
 }
 
-#CORS Django Variables
-CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
-CORS_ALLOW_CREDENTIALS = True
-#CORS_ALLOWED_ORIGINS = [
-#    '*',
-#] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
-CORS_ALLOWED_ORIGIN_REGEXES = [
-    '*',
-]
